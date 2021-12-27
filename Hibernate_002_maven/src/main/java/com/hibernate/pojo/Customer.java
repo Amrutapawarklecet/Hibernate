df052fe51a1d3bc58b5cb6ebbcdcd19fce447019
Amrutapawarklecet <96716109+Amrutapawarklecet@users.noreply.github.com>
2021-12-27 13:17:45 +0530

    Add files via upload

---
+++ Hibernate_002_maven/src/main/java/com/hibernate/pojo/Customer.java
@@ -0,0 +1,51 @@
+package com.hibernate.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer_tbl")
+public class Customer {
+	@Id
+	@Column(name="cid")
+	private int customerId;
+
+	@Column(name="cname")
+	private String customerName;
+
+	@Column(name="email")
+	private String email;
+
+	public Customer(int customerId, String customerName, String email) {
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.email = email;
+
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
