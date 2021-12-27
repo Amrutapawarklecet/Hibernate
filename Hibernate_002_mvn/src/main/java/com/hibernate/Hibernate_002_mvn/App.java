7d6c3fc7970357b75656b9d977f5a4a54a214d7b
Amrutapawarklecet <96716109+Amrutapawarklecet@users.noreply.github.com>
2021-12-27 14:49:51 +0530

    Add files via upload

---
+++ Hibernate_002_mvn/src/main/java/com/hibernate/Hibernate_002_mvn/App.java
@@ -0,0 +1,13 @@
+package com.hibernate.Hibernate_002_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
